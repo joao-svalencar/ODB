3bbba601b5d95b91a9cee455a13bbc920f10b514
joao-svalencar <joaopaulo.valencar@usp.br>
2021-05-24 21:32:27 -0300

    small change

--- git_utils.R
+++ git_utils.R
@@ -1,5 +1,17 @@
+setwd() #tem que setar antes de abrir o term
+
+git clone "link sem aspas" #clona a bagaça
+
+# compara os repos (quem foi/quem num foi)
+git status
+
+#compara os arquivos
+git diff
+# stage: preparando pra dar commit
 git add obd_agrvai.Rproj
 
+# commit: preparando pra dar o push
 git commit -m "msg do commit"
 
+# dá o push
 git push
